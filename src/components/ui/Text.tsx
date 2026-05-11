9548b4ffcc4fa1cfbd46d25ec4c003eb4985f556
NebSpacefarer <nebulaespacefarer@mail.com>
2026-05-11 18:53:32 +0200

    ✨ feat: Text component

---
+++ src/components/ui/Text.tsx
@@ -0,0 +1,23 @@
+import type { BaseHTMLAttributes } from "preact";
+import { cn } from "#utils/cn";
+
+interface TextProps extends BaseHTMLAttributes<HTMLBaseElement> {
+	size?: "normal" | "h1" | "h2" | "sm";
+}
+
+export default function Text(props: TextProps) {
+	props.size = props.size ?? "normal";
+
+	return (
+		<p
+			className={cn(
+				props.size === "h1" && "text-xl",
+				props.size === "h2" && "text-lg",
+				props.size === "sm" && "text-sm",
+				props.className,
+			)}
+		>
+			{props.children}
+		</p>
+	);
+}
