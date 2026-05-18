36cd13c9ddffc15c3480cfe07829d02a527275ca
NebSpacefarer <nebulaespacefarer@mail.com>
2026-05-18 19:03:38 +0200

    ✨ feat: PageTitle component for uniformity

---
+++ src/components/PageTitle.tsx
@@ -0,0 +1,24 @@
+import { Separator } from "@base-ui/react";
+import type { ComponentChildren } from "preact";
+import Text from "#ui/Text";
+
+export default function PageTitle({
+    pageIcon,
+    pageTitle,
+}: {
+    pageIcon: ComponentChildren;
+    pageTitle: string;
+}) {
+    return (
+        <div className="flex flex-col gap-sm">
+            <div className="flex items-center gap-xs">
+                {pageIcon}
+                <Text className="w-fit border-primary border-b-3 font-semibold text-2xl">
+                    {pageTitle}
+                </Text>
+            </div>
+
+            <Separator orientation="horizontal" className="h-px bg-border" />
+        </div>
+    );
+}
