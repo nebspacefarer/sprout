7055ad073205f77c47c741d4095b170458bbe9d6
NebSpacefarer <nebulaespacefarer@mail.com>
2026-05-19 00:32:25 +0200

    🐛 fix: Remove Triage task status

--- src/utils/status.ts
+++ src/utils/status.ts
@@ -11,11 +11,10 @@ export const projectStatuses: Status[] = [
 ];
 
 export const taskStatuses: Status[] = [
-    { id: 0, title: "Triage", color: "#B8B8B8" },
-    { id: 1, title: "Todo", color: "#B8B8B8" },
-    { id: 2, title: "In Progress", color: "#5C6DDB" },
-    { id: 3, title: "Stale", color: "#FCA465" },
-    { id: 4, title: "Review Needed", color: "#ED82DE" },
-    { id: 5, title: "Reviewed", color: "#82ED84" },
-    { id: 6, title: "Done", color: "#549C6A" },
+    { id: 0, title: "Todo", color: "#B8B8B8" },
+    { id: 1, title: "In Progress", color: "#5C6DDB" },
+    { id: 2, title: "Stale", color: "#FCA465" },
+    { id: 3, title: "Review Needed", color: "#ED82DE" },
+    { id: 4, title: "Reviewed", color: "#82ED84" },
+    { id: 5, title: "Done", color: "#549C6A" },
 ];
