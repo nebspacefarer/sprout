e83086cbcf1898c97459f00e461ac7ca06958ed3
NebSpacefarer <contact@nebspacefarer.com>
2026-05-29 23:47:29 +0200

    🐛 fix: Footer sizing and background

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,7 +1,9 @@
+import Text from "#ui/Text";
+
 export default function Footer() {
 	return (
-		<div className="fixed bottom-0 w-full">
-			<div className="text-center text-sm">🌱 Sprout - 2026 - MIT</div>
+		<div className="fixed bottom-0 w-full text-center text-sm">
+			<Text className="bg-surface">🌱 Sprout - 2026 - MIT</Text>
 		</div>
 	);
 }
